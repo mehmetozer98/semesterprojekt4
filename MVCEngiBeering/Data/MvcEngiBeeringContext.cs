ce1947887e26082083f63d5a24925ac35b16f0c4
Rasmus Thomsen <rasmt18@student.sdu.dk>
2019-10-25 09:30:06 +0200

    Comment removed

--- MVCEngiBeering/Data/MvcEngiBeeringContext.cs
+++ MVCEngiBeering/Data/MvcEngiBeeringContext.cs
@@ -12,10 +12,6 @@ namespace MVCEngiBeering.Data
                 
             }
 
-//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//            => optionsBuilder.UseNpgsql(
-//                "Host='tek-mmmi-db0a.tek.c.sdu.dk';Database='si3_2019_group_3_db';Username='si3_2019_group_3';Password='sox6.genets'");
-
         public DbSet<BBMachine> machines { get; set; }
         public DbSet<MachineState> statetypes { get; set; }
         public DbSet<ProductType> producttypes { get; set; }
