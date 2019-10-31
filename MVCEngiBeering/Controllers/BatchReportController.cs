76064d31bd74941cdc66b2c5be7b1a564b29b503
Rasmus Thomsen <rasmt18@student.sdu.dk>
2019-10-31 10:26:57 +0100

    Added pages

---
+++ MVCEngiBeering/Controllers/BatchReportController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVCEngiBeering.Controllers
+{
+    public class BatchReportController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Get(int id)
+        {
+            return View();
+        }
+    }
+}
