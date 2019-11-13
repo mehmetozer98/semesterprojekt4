cef232d020e2534d9d018c13558ed485a82273f5
Søren Pedersen <rodsur@rodsur.net>
2019-11-13 10:36:59 +0100

    omdøbt goodamount til actualamount

--- MVCEngiBeering/Models/Batch.cs
+++ MVCEngiBeering/Models/Batch.cs
@@ -6,7 +6,7 @@ namespace MVCEngiBeering.Models
         public BBMachine machine { get; set; }
         public ProductType producttype { get; set; }
         public int totalamount { get; set; }
-        public int goodamount { get; set; }
+        public int actualamount { get; set; }
         public int defectamount { get; set; }
     }
 }
