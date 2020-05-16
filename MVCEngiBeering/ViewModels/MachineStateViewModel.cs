2aee64e8103786331319fc8e9d90e8496023e71e
Mehmet Øzer <meuze18@student.sdu.dk>
2020-05-16 03:11:33 +0200

    slet alt kode

--- MVCEngiBeering/ViewModels/MachineStateViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace MVCEngiBeering.ViewModels
-{
-    public class MachineStateViewModel
-    {
-        public int id { get; set; }
-        public string name { get; set; }
-    }
-}
--- MVCEngiBeering/ViewModels/ProductTypeViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace MVCEngiBeering.ViewModels
-{
-    public class ProductTypeViewModel
-    {
-        public int id { get; set; }
-        public string name { get; set; }
-    }
-}
